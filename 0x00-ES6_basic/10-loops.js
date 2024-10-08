64ea406c93e73a1321d83791382b77a2638129a3
Mahmoud Ismail <ismailm7373@gmail.com>
2024-10-09 01:29:32 +0300

    10-loops.js

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const new_array = [];
-  for (let value of array) {
-    new_array.push(appendString + value);
+  const newArray = [];
+  for (const value of array) {
+    newArray.push(appendString + value);
   }
 
-  return new_array;
+  return newArray;
 }
