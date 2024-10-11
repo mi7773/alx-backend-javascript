fbe63c96979363878d30d28631ae79c9893be376
Mahmoud Ismail <ismailm7373@gmail.com>
2024-10-12 02:38:21 +0300

    1. Let's build a Teacher interface

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,13 @@
+interface Teacher {
+
+  readonly firstName: string;
+  readonly lastName: string;
+
+  fullTimeEmployee: boolean;
+
+  yearsOfExperience?: number;
+
+  location: string;
+
+  [key: string]: any;
+}
