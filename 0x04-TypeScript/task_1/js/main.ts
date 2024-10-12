777740ba135ba734f55f61883f861a1df18a261e
Mahmoud Ismail <ismailm7373@gmail.com>
2024-10-12 04:41:40 +0300

    4. Writing a class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -17,11 +17,44 @@ interface Directors extends Teacher {
   numberOfReports: number;
 }
 
-interface printTeacherFunction {
+interface PrintTeacherFunction {
 
   (firstName: string, lastName: string): string;
 }
 
-const printTeacher: printTeacherFunction = (firstName: string, lastName: string) => {
+const printTeacher: PrintTeacherFunction = (firstName: string, lastName: string): string => {
   return `${firstName.charAt(0)}. ${lastName}`;
 };
+
+interface Student {
+
+  firstName: string;
+  lastName: string;
+
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+interface StudentConstructor {
+
+  new (firstName: string, lastName: string): Student;
+}
+
+class StudentClass implements Student {
+
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
