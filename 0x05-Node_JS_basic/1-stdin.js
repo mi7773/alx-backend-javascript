4c88e1216ab891459784de95eda0608263a30c8b
Mahmoud <ismailm7373@gmail.com>
2024-11-27 01:12:34 +0200

    1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -9,11 +9,6 @@ const rl = readline.createInterface({
 
 rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
   console.log(`Your name is: ${name}`);
+  console.log('This important software is now closing');
   rl.close();
-});
-
-rl.on('close', () => {
-  if (isInteractive === false) {
-    console.log('This important software is now closing');
-  }
 });
