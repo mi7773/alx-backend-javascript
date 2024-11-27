b09ac380a758ba379d2056d7c187734eb5403405
Mahmoud <ismailm7373@gmail.com>
2024-11-27 02:57:27 +0200

    7-http_express.js

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+  res.send('This is the list of our students');
+});
+
+app.listen(1245);
+
+module.exports = app;
