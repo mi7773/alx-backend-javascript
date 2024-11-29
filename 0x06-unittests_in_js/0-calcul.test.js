40a55ee1d7b21c236af7ef8efb2e0e4db7e18dc6
Mahmoud <ismailm7373@gmail.com>
2024-11-29 02:06:56 +0200

    0-calcul.js

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+const calculateNumber = require('./0-calcul');
+
+describe('CalculateNumber', () => {
+  it('1 + 3 should return 4', () => {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+  });
+
+  it('1 + 3.7 should return 5', () => {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  });
+
+  it('1.2 + 3.7 should return 5', () => {
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  });
+
+  it('1.5 + 3.7 should return 6', () => {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+});
