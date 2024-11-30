656ecd9c5cc207f8d94725b2c5961151dd7dda72
Mahmoud <ismailm7373@gmail.com>
2024-11-30 05:14:11 +0200

    5-http.js

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,12 +1,28 @@
 const http = require('http');
+const countStudent = require('./3-read_file_async');
 
-const app = http.createServer((req, res) => {
+const originalConsoleLog = console.log;
+
+let consoleOutput = '';
+
+console.log = (message) => {
+  consoleOutput += `${message}\n`;
+};
+
+const app = http.createServer(async (req, res) => {
   if (req.method === 'GET' && req.url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
   } else if (req.method === 'GET' && req.url === '/students') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('This is the list of our students');
+    try {
+      await countStudent(process.argv[2]);
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      const output = `This is the list of our students\n${consoleOutput}`;
+      res.end(output);
+    } finally {
+      console.log = originalConsoleLog;
+      consoleOutput = '';
+    }
   }
 });
 
