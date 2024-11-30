3dedddf99388d73871e9ea85ee218f6cdb085da1
Mahmoud <ismailm7373@gmail.com>
2024-11-30 04:34:30 +0200

    2-read_file.js

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+function countStudent(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+    // const headers = lines[0].split(',');
+    const students = lines.slice(1);
+    const fields = {};
+    for (const student of students) {
+      const field = student.split(',')[3];
+      const firstName = student.split(',')[0];
+      if (!(field in fields)) {
+        fields[field] = {
+          num: 1,
+          firstNames: [firstName],
+        };
+      } else {
+        fields[field].num += 1;
+        fields[field].firstNames.push(firstName);
+      }
+    }
+    console.log(`Number of students: ${students.length}`);
+    for (const field in fields) {
+      if (Object.prototype.hasOwnProperty.call(fields, field)) {
+        console.log(`Number of students in ${field}: ${fields[field].num}. List: ${fields[field].firstNames.join(', ')}`);
+      }
+    }
+  } catch (_) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudent;
